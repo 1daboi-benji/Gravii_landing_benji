edb9cbac087231c2d01024c471a87ccff26069d7
1daboi-benji <benji@gravii.xyz>
2026-03-06 02:04:47 +0900

    feat: add GraviiLanding page with Vite+React setup

---
+++ main.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import GraviiLanding from "./GraviiLanding.jsx";
+
+createRoot(document.getElementById("root")).render(<GraviiLanding />);
